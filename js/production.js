76389def45ebff1cbe35feefc7b4696ebf71bca5
Jay L Greentree <jaylgreentree@Jays-Mac-mini.local>
2018-01-02 18:31:41 -0500

    Fix counter

--- js/production.js
+++ js/production.js
@@ -766,7 +766,7 @@ jQuery(function() {
               }
             }
           }
-          $('[data-churchonline-counter]').text("in " + (days == "0" ? "" : (days + " days ")) + (hrs == "0" ? "" : (hours + " hours ")) + (minutes == "0" ? "" : (minutes + " mins ")) + (seconds + "secs"))
+          $('[data-churchonline-counter]').text("in " + (days == "0" ? "" : (days + "days ")) + (hours == "0" ? "" : (hours + "hours ")) + (minutes == "0" ? "" : (minutes + "mins ")) + (seconds + "secs"))
           if (seconds === 0 && minutes === 0 && hours === 0 && days === 0) {
             goLive();
             return clearInterval(intervalId);
